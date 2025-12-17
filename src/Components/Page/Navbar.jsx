9e93641047df0dbf583cf49f42b198210a61e10a
Develope <a@gmial.com>
2025-12-17 19:36:22 +0700

    feature(navbar): still develop on navbar not complete

--- src/Components/Page/Navbar.jsx
+++ src/Components/Page/Navbar.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return <>
   <div className='Navbar flex max-w-full bg-green-600 h-[44px] items-center '>
-         <ul className='menu flex max-w-[1000px]  mx-auto  gap-x-[15px] bg-red-500 relative  ' >
+         <ul className='menu flex max-w-[1000px]  mx-auto  gap-x-[15px] bg-red-500   ' >
             <li><a href="">LoGO</a></li>
 
-            <li className='relative group'>
+            <li className=' group'>
                 <a href="">Store</a>
-                <div className=' absolute w-[891px] 
+                <div className=' absolute w-full 
+                left-0
                 min-h[125px]
                  bg-blue-600
                   hidden 
@@ -215,9 +216,296 @@ const Navbar = () => {
              </div>
             
             
+            </li>
+           
+           
+           
+           
+            <li className='relative group'>
+            <a href="">Watch</a>
+            <div className=' absolute w-[891px] 
+                min-h[125px]
+                 bg-blue-600
+                  hidden 
+                  group-hover:flex
+                  '>
+               <div>
+                <ul>
+                    <li><a href="">Explore Watch</a></li>
+                    <li><a href="">Explore All Apple Watch</a></li>
+                    <li><a href="">Aple Watch Series 11</a></li>
+                    <li><a href="">Apple Watch SE 3</a></li>
+                    <li><a href="">Apple Watch ULtra 3</a></li>
+                    <li><a href="">Apple Watch Nike </a></li>
+                    <li><a href="">Apple Watch Hermes</a></li>
+                    <li><a href="">compare Watch</a></li>
+                    <li><a href="">Why Apple Watch</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">Shop Watch </a></li>
+                    <li><a href="">shop Apple Watch</a></li>
+                    <li><a href="">Apple Watch Bands</a></li>
+                    <li><a href="">Apple Watch Accessories</a></li>
+                    <li><a href="">Apple Trade In</a></li>
+                    <li><a href="">Financing</a></li>
+                    <li><a href="">Personal Setupd</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">More from Watch</a></li>
+                    <li><a href="">Apple Watch Support</a></li>
+                    <li><a href="">AppleCare</a></li>
+                    <li><a href="">watchOS 26</a></li>
+                    <li><a href="">Apple Watch FOr YOur Kids</a></li>
+                    <li><a href="">Apps by Apple</a></li>
+                    <li><a href="">Apple Finess+</a></li>
+                    <li><a href="">iCloud+</a></li>
+                    <li><a href="">Mac for Business</a></li>
+                    <li><a href="">Education</a></li>
+                </ul>
+                </div>
+             </div>  
+            </li>
+          
+
+          
+          
+            <li className='relative group'>
+            <a href="">Vision</a>
+            <div className=' absolute w-[891px] 
+                min-h[125px]
+                 bg-blue-600
+                  hidden 
+                  group-hover:flex
+                  '>
+               <div>
+                <ul>
+                    <li><a href="">Explore Watch</a></li>
+                    <h1>Explore Apple Vision Pro</h1>
+                    <li><a href="">Tech Specs</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">Shop Vision </a></li>
+                    <li><a href="">shop Apple Vision Pro</a></li>
+                    <li><a href="">Apple Vision Pro </a></li>
+                    <li><a href="">Accessories</a></li>
+                    <li><a href="">Book a Demo</a></li>
+                    <li><a href="">Financing</a></li>
+                    <li><a href="">Personal Setup</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">More from Vision </a></li>
+                    <li><a href="">Apple Vision Pro Support</a></li>
+                    <li><a href="">AppleCare</a></li>
+                    <li><a href="">visionOs 26</a></li>
+                </ul>
+                </div>
+             </div>
+            </li>
+          
+          
+
+          
+            <li className='relative group'>
+            <a href="">Airpods</a>
+            <div className=' absolute w-[891px] 
+                min-h[125px]
+                 bg-blue-600
+                  hidden 
+                  group-hover:flex
+                  '>
+               <div>
+                <ul>
+                    <li><a href="">Explore Airpods</a></li>
+                    <li><a href="">AirPods 4</a></li>
+                    <li><a href="">AirPods Pro 3</a></li>
+                    <li><a href="">AirPods Max</a></li>
+                    <li><a href="">compare Airpods</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">Shop Airpods </a></li>
+                    <li><a href="">shop Airpods</a></li>
+                    <li><a href="">Airpods Accessories</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">More from Airpods</a></li>
+                    <li><a href="">Airpods Support</a></li>
+                    <li><a href="">AppleCare</a></li>
+                    <li><a href="">Hearing Health</a></li>
+                    <li><a href="">Apple Music</a></li>
+                    <li><a href="">Fitness+</a></li>
+                </ul>
+                </div>
+             </div>
+            </li>
             
+                 
             
-            
+            <li className='relative group'>
+            <a href="">TV& Home</a>
+            <div className=' absolute w-[891px] 
+                min-h[125px]
+                 bg-blue-600
+                  hidden 
+                  group-hover:flex
+                  '>
+               <div>
+                <ul>
+                    <li><a href="">Explore TV & Home</a></li>
+                    <li><a href="">Explore All TV & Home</a></li>
+                    <li><a href="">Apple TV 4K</a></li>
+                    <li><a href="">HomePod </a></li>
+                    <li><a href="">HomePod mini</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">Shop Tv & Home </a></li>
+                    <li><a href="">Shop Apple TV 4K</a></li>
+                    <li><a href="">Shop HomePod </a></li>
+                    <li><a href="">Shop HomePod mini</a></li>
+                    <li><a href="">shop Siri Remote</a></li>
+                    <li><a href="">Tv & Home Accessories</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">More from TV & Home</a></li>
+                    <li><a href="">Airpods TV & Home Support</a></li>
+                    <li><a href="">HomePod Support</a></li>
+                    <li><a href="">AppleCare for Apple TV</a></li>
+                    <li><a href="">AppleCare for HomePod</a></li>
+                    <li><a href="">Apple TV app</a></li>
+                    <li><a href="">Apple TV</a></li>
+                    <li><a href="">HOme app</a></li>
+                    <li><a href="">Apple Music</a></li>
+                    <li><a href="">Siri</a></li>
+                    <li><a href="">AirPlay</a></li>
+                </ul>
+                </div>
+             </div>
+            </li>
+
+
+
+            <li className='relative group'>
+            <a href="">Entertainment</a>
+            <div className=' absolute w-[891px] 
+                min-h[125px]
+                 bg-blue-600
+                  hidden 
+                  group-hover:flex
+                  '>
+               <div>
+                <ul>
+                    <li><a href="">Explore Entertainment</a></li>
+                    <li><a href="">Explore Entertainment</a></li>
+                    <li><a href="">Apple One</a></li>
+                    <li><a href="">Apple Music </a></li>
+                    <li><a href="">Apple Arcade</a></li>
+                    <li><a href="">Apple Fitness+</a></li>
+                    <li><a href="">Apple New+</a></li>
+                    <li><a href="">Apple Podcasts</a></li>
+                    <li><a href="">Apple Books</a></li>
+                    <li><a href="">Apple Store</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">Support</a></li>
+                    <li><a href="">Apple TV Support</a></li>
+                    <li><a href="">Apple Music Support</a></li>
+                </ul>
+                </div>
+             </div>       
+            </li>
+           
+
+           
+            <li className='relative group'>
+            <a href="">Accessories</a>
+            <div className=' absolute w-[891px] 
+                min-h[125px]
+                 bg-blue-600
+                  hidden 
+                  group-hover:flex
+                  '>
+               <div>
+                <ul>
+                    <li><a href="">Shop All Accessories</a></li>
+                    <li><a href="">Mac</a></li>
+                    <li><a href="">ipad</a></li>
+                    <li><a href="">iPhone</a></li>
+                    <li><a href="">Apple Watch</a></li>
+                    <li><a href="">Applw Vision Pro</a></li>
+                    <li><a href="">AirPods</a></li>
+                    <li><a href="">AirPods</a></li>
+                    <li><a href="">TV & Home</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">Explore Accessories</a></li>
+                    <li><a href="">Made by Apple</a></li>
+                    <li><a href="">Beats</a></li>
+                    <li><a href="">AirTag</a></li>
+                    <li><a href="">Assistive Technologies</a></li>
+                </ul>
+                </div>
+             </div> 
+            </li>
+   
+          
+            <li className='relative group'>
+            <a href="">Support</a>
+            <div className=' absolute w-[891px] 
+                min-h[125px]
+                 bg-blue-600
+                  hidden 
+                  group-hover:flex
+                  '>
+               <div>
+                <ul>
+                    <li><a href="">Explore Support</a></li>
+                    <li><a href="">Mac</a></li>
+                    <li><a href="">Watch</a></li>
+                    <li><a href="">Apple Vision Pro</a></li>
+                    <li><a href="">AirPods</a></li>
+                    <li><a href="">Music</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">Explore Support</a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">Get Help</a></li>
+                    <li><a href="">Community</a></li>
+                    <li><a href="">Check Converage</a></li>
+                    <li><a href="">Genius Bar</a></li>
+                    <li><a href=""></a></li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                    <li><a href="">Helpful Topics</a></li>
+                    <li><a href="">Get AppleCare</a></li>
+                    <li><a href="">Applw Account and <br /> Password</a></li>
+                    <li><a href="">Billing & Subscriptions</a></li>
+                    <li><a href=""></a></li>
+                </ul>
+                </div>
+             </div>
             
             
             
@@ -248,45 +536,30 @@ const Navbar = () => {
             
             
             </li>
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-            <li><a href="">Watch</a></li>
-            <li><a href="">Vision</a></li>
-            <li><a href="">Airpods</a></li>
-            <li><a href="">TV& Home</a></li>
-            <li><a href="">Entertainment</a></li>
-            <li><a href="">Accessories</a></li>
-            <li><a href="">Support</a></li>
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
             <li><a href="">Search</a></li>
             <li><a href="">Shop</a></li>
          </ul>
